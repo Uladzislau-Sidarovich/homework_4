8b7eb41a7c7e3d1edd63bb824f27f315f8cda949
Uladzislau <juniors.fryt@gmail.com>
2023-03-10 16:05:02 +0300

    added new file and task

---
+++ task027/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert. ToInt32(Console. ReadLine ());
+int originalNumber = number;
+int sum = 0;
+while(number > 0)
+{
+sum = sum + number%10;
+number = number/10;
+}
+Console.WriteLine($"Сумма всех цифр в числе {originalNumber} равна {sum}");
---
+++ task029/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = 8;
+int[] array = new int[n];
+Random arr = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = arr.Next(1, 9);
+}
+Console.WriteLine($"[{String.Join("; ", array)}]");
